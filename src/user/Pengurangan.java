ca363ad3985c4220235c1abdc73d6ca6ffb2fdf4
Naufal Ammar <amfal11@users.noreply.github.com>
2019-11-13 22:51:45 +0700

    Update Pengurangan.java

--- src/user/Pengurangan.java
+++ src/user/Pengurangan.java
@@ -4,11 +4,41 @@
  * and open the template in the editor.
  */
 package user;
+import java.util.Random;
 
 /**
  *
- * @author laptop
+ * @author Naubyra (Naufal Ammar)
  */
 public class Pengurangan {
+    private int A, B;
+    public int hasil;
+    Random rand = new Random();
     
+    public void randomSoal() {
+        switch (level) {
+            case 1: // Random number dari 0 - 10
+                A = rand.nextInt(11);
+                B = rand.nextInt(11);
+                if (B > A) {
+                    hasil = B - A;
+                }
+                else {
+                    hasil = A - B;
+                }
+                break;
+            case 2:
+                A = -10 + rand.nextInt(10);
+                B = -10 + rand.nextInt(10);
+                hasil = A - B;
+                break;
+            case 3:
+                A = -10 + rand.nextInt(21);
+                B = -10 + rand.nextInt(21);
+                hasil = A - B;
+                break;
+            default:
+                break;
+        }
+    }
 }
