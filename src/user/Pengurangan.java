758e442a947b075eeeaa0b9a525e82078fa2c4d5
Naufal Ammar <amfal11@users.noreply.github.com>
2019-11-14 12:01:09 +0700

    Update Pengurangan.java
    Penambahan komentar dan sedikit perbaikan

--- src/user/Pengurangan.java
+++ src/user/Pengurangan.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package user;
+
+// Import
 import java.util.Random;
 
 /**
@@ -14,82 +16,81 @@ public class Pengurangan extends User{
     private Integer A, B;
     public int hasil;
     private String teksA, teksB;
+
+    // Instansiasi Random
     Random rand = new Random();
     
-    public void setAngkaA() {
+    // Method Set
+    public void setAngkaA() { // Set Integer A sesuai dengan Level
         switch (Level) {
-            case 1:
-                A = rand.nextInt(11);
+            case 1: // Jika Level = 1
+                A = rand.nextInt(11); // Set Integer dengan angka 0 - 10
                 break;
-            case 2:
-                A = -10 + rand.nextInt(10);
+            case 2: // Jika Level = 2
+                A = -10 + rand.nextInt(10); // Set Integer dengan angka (-10) - (-1)
                 break;
-            case 3:
-                A = -10 + rand.nextInt(21);
+            case 3: // Jika Level = 3
+                A = -10 + rand.nextInt(21); // Set Integer dengan angka (-10) - (10)
                 break;
             default:
                 break;
         }
     }
     
-    public void setAngkaB() {
+    public void setAngkaB() { // Set Integer B sesuai dengan Level
         switch (Level) {
-            case 1:
-                B = rand.nextInt(11);
+            case 1: // Jika Level = 1
+                B = rand.nextInt(11); // Set Integer dengan angka 0 - 10
                 break;
-            case 2:
-                B = -10 + rand.nextInt(10);
+            case 2: // Jika Level = 2
+                B = -10 + rand.nextInt(10); // Set Integer dengan angka (-10) - (-1)
                 break;
-            case 3:
-                B = -10 + rand.nextInt(21);
+            case 3: // Jika Level = 3
+                B = -10 + rand.nextInt(21); // Set Integer dengan angka (-10) - (10)
                 break;
             default:
                 break;
         }
     }
     
-    public int getAngkaA() {
-        return A;
-    }
-    
-    public int getAngkaB() {
-        return B;
-    }
-    
-    public String getTeksA() {
+    // Method Get
+    public String getTeksA() { // Mendapatkan Teks A
         if (A < 0) {
-           teksA = "(" + A.toString() + ")";
+           teksA = "(" + A.toString() + ")"; // Jika A < 0 Maka Teks A = (A)
         }
         else {
-           teksA = A.toString();
+           teksA = A.toString(); // Jika A !< 0 Maka Teks A = A
         }
         return teksA;
     }
     
-    public String getTeksB() {
+    public String getTeksB() { // Mendapatkan Teks B
         if (B < 0) {
-           teksB = "(" + B.toString() + ")";
+           teksB = "(" + B.toString() + ")"; // Jika B < 0 Maka Teks B = (B)
         }
         else {
-           teksB = B.toString();
+           teksB = B.toString(); // Jika B !< 0 Maka Teks B = B
         }
         return teksB;
     }
     
     public void randomSoal() {
+        // Merandom Angka A dan B
         this.setAngkaA();
         this.setAngkaB();
-        this.getAngkaA();
-        this.getAngkaB();
-        this.getTeksA();
-        this.getTeksB();
-        if (Level == 1 && A < B) {
-            System.out.println("Berapakah hasil dari " + B + " - " + A + " ?");
+
+        if (Level == 1 && A < B) { // Jika Level 1 Mempunyai Angka B > A Maka B - A Agar Hasil tidak negatif
+            System.out.println("Berapakah hasil dari " + this.getTeksB() + " - " + this.getTeksA() + " ?");
             hasil = B - A;
         }
         else {
-            System.out.println("Berapakah hasil dari " + A + " - " + B + " ?");
+            System.out.println("Berapakah hasil dari " + this.getTeksA() + " - " + this.getTeksB() + " ?");
             hasil = A - B;
         }
+    }
+    
+    public static void main(String[] args) {
+        Pengurangan p = new Pengurangan();
+        p.randomSoal();
     }
 }
