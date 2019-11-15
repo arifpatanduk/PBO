e84b6b5b98c02cdd2c66d78d647a947209f8c0ca
erixprasetyo27 <57607603+erixprasetyo27@users.noreply.github.com>
2019-11-15 13:49:52 +0700

    Update Pengurangan

--- src/user/Pengurangan.java
+++ src/user/Pengurangan.java
@@ -4,19 +4,12 @@
  * and open the template in the editor.
  */
 package user;
-<<<<<<< HEAD
-import java.util.*;
-=======
-
 // Import
 import java.util.Random;
-
->>>>>>> 6e69c38924360838bc5d7e1c4aac2b06367b05ce
 /**
  *
  * @author Erix Prasetyo Waluyo
  */
-<<<<<<< HEAD
 public class Pengurangan extends Penjumlahan {
     @Override
     public void randomSoal(){
@@ -50,82 +43,6 @@ public class Pengurangan extends Penjumlahan {
                 System.out.println("Berapakah hasil dari " + soalA + " - " + soalB + " ?");
             }
             int hasil = soalA - soalB;
-=======
-public class Pengurangan extends User{
-    private Integer A, B;
-    public int hasil;
-    private String teksA, teksB;
-
-    // Instansiasi Random
-    Random rand = new Random();
-    
-    // Method Set
-    public void setAngkaA() { // Set Integer A sesuai dengan Level
-        switch (Level) {
-            case 1: // Jika Level = 1
-                A = rand.nextInt(11); // Set Integer dengan angka 0 - 10
-                break;
-            case 2: // Jika Level = 2
-                A = -10 + rand.nextInt(10); // Set Integer dengan angka (-10) - (-1)
-                break;
-            case 3: // Jika Level = 3
-                A = -10 + rand.nextInt(21); // Set Integer dengan angka (-10) - (10)
-                break;
-            default:
-                break;
->>>>>>> 6e69c38924360838bc5d7e1c4aac2b06367b05ce
-        }
-    }
-    
-    public void setAngkaB() { // Set Integer B sesuai dengan Level
-        switch (Level) {
-            case 1: // Jika Level = 1
-                B = rand.nextInt(11); // Set Integer dengan angka 0 - 10
-                break;
-            case 2: // Jika Level = 2
-                B = -10 + rand.nextInt(10); // Set Integer dengan angka (-10) - (-1)
-                break;
-            case 3: // Jika Level = 3
-                B = -10 + rand.nextInt(21); // Set Integer dengan angka (-10) - (10)
-                break;
-            default:
-                break;
-        }
-    }
-    
-    // Method Get
-    public String getTeksA() { // Mendapatkan Teks A
-        if (A < 0) {
-           teksA = "(" + A.toString() + ")"; // Jika A < 0 Maka Teks A = (A)
-        }
-        else {
-           teksA = A.toString(); // Jika A !< 0 Maka Teks A = A
-        }
-        return teksA;
-    }
-    
-    public String getTeksB() { // Mendapatkan Teks B
-        if (B < 0) {
-           teksB = "(" + B.toString() + ")"; // Jika B < 0 Maka Teks B = (B)
-        }
-        else {
-           teksB = B.toString(); // Jika B !< 0 Maka Teks B = B
-        }
-        return teksB;
-    }
-    
-    public void randomSoal() {
-        // Merandom Angka A dan B
-        this.setAngkaA();
-        this.setAngkaB();
-
-        if (Level == 1 && A < B) { // Jika Level 1 Mempunyai Angka B > A Maka B - A Agar Hasil tidak negatif
-            System.out.println("Berapakah hasil dari " + this.getTeksB() + " - " + this.getTeksA() + " ?");
-            hasil = B - A;
-        }
-        else {
-            System.out.println("Berapakah hasil dari " + this.getTeksA() + " - " + this.getTeksB() + " ?");
-            hasil = A - B;
         }
     }
 }
